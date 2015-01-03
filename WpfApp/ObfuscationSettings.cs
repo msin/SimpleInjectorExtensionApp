b27b7b6e2518807e6be8f9bfe84706af044e274c
mike_shubin <mike_shubin@mail.ru>
2015-01-03 14:50:05 +0300

    Applied obfuscation without type renaming

---
+++ WpfApp/ObfuscationSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: Obfuscation(Feature = "encrypt symbol names with password 'TestPassword'", Exclude = false)]
+[assembly: Obfuscation(Feature = "string encryption", Exclude = false)]
+[assembly: Obfuscation(Feature = "Apply to type WpfApp.*: renaming", Exclude = true, ApplyToMembers = true)]
+[assembly: Obfuscation(Feature = "encrypt resources [compress]", Exclude = false)]
+[assembly: Obfuscation(Feature = "embed SimpleInjector.dll", Exclude = false)]
+[assembly: Obfuscation(Feature = "embed DevExpress.Mvvm.v14.2.dll", Exclude = false)]
+
